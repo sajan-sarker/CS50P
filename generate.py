5a114f3b32c1aff24dbe554f6b1cf4413e4095cb
Sajan Kumer Sarker <sajansarker47@gmail.com>
2024-01-03 14:19:33 +0600

    libraries

---
+++ generate.py
@@ -0,0 +1,20 @@
+import random
+#from random import choice
+
+#coin = random.choice(['H', 'T'])    
+#coin = choice(['H', 'T'])
+#number = random.randint(1, 10)
+'''cards = ['jack', 'queen', 'king', 'ace', 'joker']
+random.shuffle(cards)'''
+
+#print(cards)
+#print(coin)
+#print(number)
+
+'''for card in cards:
+    print(card)'''
+
+
+
+
+
